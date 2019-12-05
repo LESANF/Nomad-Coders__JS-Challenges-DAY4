34b42f24499297b03adb1f71b815133fcd05acd1
LESANF <nagong1000@naver.com>
2019-12-06 01:22:14 +0900

    END

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 // <⚠️ DONT DELETE THIS ⚠️>
 // import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
+
+const body = document.querySelector("body");
+body.style.backgroundColor = "#9b59b6";
+
+function handleResize() {
+  const WIDTH = window.innerWidth;
+  if (WIDTH >= 1000) {
+    body.style.backgroundColor = "#EEBC12";
+  } else if (700 < WIDTH && WIDTH < 1000) {
+    body.style.backgroundColor = "#9b59b6";
+  } else {
+    body.style.backgroundColor = "#2D8ED6";
+  }
+}
+
+window.addEventListener("resize", handleResize);

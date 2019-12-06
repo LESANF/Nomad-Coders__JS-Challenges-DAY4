7164a7733d94b44efd311a4bbf8110aea5f1dd33
LESANF <nagong1000@naver.com>
2019-12-06 13:50:23 +0900

    END

--- src/index.js
+++ src/index.js
@@ -7,16 +7,23 @@ const h2 = document.querySelector("h2");
 
 h2.innerText = "Hello!";
 h2.style.color = "white";
-body.style.backgroundColor = "#9b59b6";
+
+const BIG_SCREEN = "bigScreen";
+const MEDIUM_SCREEN = "mediumScreen";
+const SMALL_SCREEN = "smallScreen";
 
 function handleResize() {
-  const WIDTH = window.innerWidth;
-  if (WIDTH >= 1000) {
-    body.style.backgroundColor = "#EEBC12";
-  } else if (700 < WIDTH && WIDTH < 1000) {
-    body.style.backgroundColor = "#9b59b6";
+  const width = window.innerWidth;
+
+  if (width > 1000) {
+    body.classList.add(BIG_SCREEN);
+    body.classList.remove(MEDIUM_SCREEN);
+  } else if (700 < width && width <= 1000) {
+    body.classList.add(MEDIUM_SCREEN);
+    body.classList.remove(BIG_SCREEN, SMALL_SCREEN);
   } else {
-    body.style.backgroundColor = "#2D8ED6";
+    body.classList.remove(MEDIUM_SCREEN);
+    body.classList.add(SMALL_SCREEN);
   }
 }
 
